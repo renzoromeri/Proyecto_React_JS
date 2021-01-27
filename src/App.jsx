c3433d19f3fb9dfcd6ee1fe6a102fb500edd2104
renzoromeri <renzo.romeri@gmail.com>
2021-01-27 17:46:24 -0300

    subo comienzo de navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/navbar';
+
+const App = () => {
+  return (
+    <>    
+    <NavbarComponent />
+
+    <button className="btn btn-primary btn-success">Exito!</button>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,41 @@
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavbarComponent = () => {
+  return (
+    <>
+
+        <nav>
+            <h2>La Botellita Vinoteca</h2>
+            <section>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="/#">Vinos</a>
+                        </li>
+                        <li>
+                            <a href="/#">Cervezas</a>
+                        </li>
+                        <li>
+                            <a href="/#">Gaseosas</a>
+                        </li>
+                        <li>
+                            <a href="/#">Whiskys</a>
+                        </li>
+                        <li>
+                            <a href="/#">Espirituosos</a>
+                        </li>
+                        <li>
+                            <a href="/#">Accesorios</a>
+                        </li>
+                        <li>
+                            <a href="/#">Promociones</a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </nav>
+    </>
+  );
+}
+
+export default NavbarComponent;
